10c2d823aa9ad047079d0bc71078d3a2469bc146
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-03-23 10:51:31 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,30 @@ const App = () => {
   return (
     <LocationProvider>
       <WeatherProvider>
-        <header className="w-full min-w-full p-4 mb-4 text-xl font-bold text-gray-100 bg-gray-500 shadow">
+        <header className="p-4 mb-4 text-xl font-bold text-gray-100 bg-gray-500 shadow">
           <a href="https://shnk.tech">SHNK.TECH</a>
         </header>
-        <div className="max-w-screen-lg m-auto">
+        <div className="flex-1 max-w-screen-lg m-auto">
           <div className="grid gap-4 p-2 md:grid-cols-2 lg:grid-cols-3">
             <SearchCard />
             <WeatherCard />
             <div className="max-w-md p-4 bg-gray-300 rounded shadow md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1 lg:max-w-full">
               <WeatherChart />
             </div>
+          </div>
+        </div>
+        <div className="p-4 mt-4 text-gray-100 bg-gray-500 shadow">
+          <div className="flex justify-between text-xs">
+            <span>
+              <a href="https://www.freepik.com" title="Freepik">
+                Icons made by Freepik
+              </a>
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                {' '}
+                from www.flaticon.com
+              </a>
+            </span>
+            <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
           </div>
         </div>
       </WeatherProvider>
