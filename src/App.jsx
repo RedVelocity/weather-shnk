a8549eeb192611f50cd082fd6439d6bcee6e55cd
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-03-23 10:33:30 +0530

    wrapped weather in div

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,16 @@ const App = () => {
   return (
     <LocationProvider>
       <WeatherProvider>
-        <header className="w-full min-w-full p-4 mb-4 text-xl font-bold text-gray-100 bg-gray-500 rounded-b shadow">
+        <header className="w-full min-w-full p-4 mb-4 text-xl font-bold text-gray-100 bg-gray-500 shadow">
           <a href="https://shnk.tech">SHNK.TECH</a>
         </header>
-        <div className="grid gap-4 p-2 md:grid-cols-2 lg:grid-cols-3">
-          <SearchCard />
-          <WeatherCard />
-          <div className="max-w-md p-4 bg-gray-300 rounded shadow md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1 lg:max-w-full">
-            <WeatherChart />
+        <div className="max-w-screen-lg m-auto">
+          <div className="grid gap-4 p-2 md:grid-cols-2 lg:grid-cols-3">
+            <SearchCard />
+            <WeatherCard />
+            <div className="max-w-md p-4 bg-gray-300 rounded shadow md:col-start-2 md:row-span-2 md:col-span-2 md:row-start-1 lg:max-w-full">
+              <WeatherChart />
+            </div>
           </div>
         </div>
       </WeatherProvider>
