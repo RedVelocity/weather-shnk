2c66204acb0bddbb64ef73abafa566743f6e2df9
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-04-29 19:09:33 +0530

    fixed header/footer padding

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="p-4 text-3xl font-bold text-dark">
-      <div className="max-w-screen-lg m-auto">
+    <header className="text-3xl font-bold text-dark">
+      <div className="max-w-screen-lg p-4 m-auto">
         <a href="https://shnk.tech">SHNK.TECH</a>
       </div>
     </header>
