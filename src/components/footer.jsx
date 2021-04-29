b50d4c6aa51f79ec79a083bb8f83fb57efd312a8
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-04-29 19:04:50 +0530

    moved logo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="p-4 font-semibold text-dark">
-      <div className="max-w-screen-lg px-4 m-auto">
+      <div className="max-w-screen-lg m-auto">
         <a href="https://openweathermap.org/">Powered by OpenWeather</a>
       </div>
     </footer>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="p-4 text-3xl font-bold text-dark">
-      <div className="max-w-screen-lg px-4 m-auto">
+      <div className="max-w-screen-lg m-auto">
         <a href="https://shnk.tech">SHNK.TECH</a>
       </div>
     </header>
