05f842b115a5abf78dbe990ed2b57768893c289c
Shah Nawaz Khan <blooshaanster@gmail.com>
2021-05-22 08:48:37 +0530

    added weather map layer

--- src/components/WeatherMap.jsx
+++ src/components/WeatherMap.jsx
@@ -23,11 +23,37 @@ const WeatherMap = () => {
       zoom: 10,
       interactive: false,
     });
+    // add marker
     new mapboxgl.Marker({ color: '#EF4444' })
       .setLngLat([longitude, latitude])
       .addTo(map);
     // add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    // add weather layer
+    map.on('load', () => {
+      const layers = map.getStyle().layers;
+      // Find the index of the first symbol layer in the map style
+      let firstSymbolId;
+      layers.some((layer) => {
+        if (layer.type === 'symbol') {
+          firstSymbolId = layer.id;
+          return true;
+        } else return false;
+      });
+      map.addLayer(
+        {
+          id: 'owm',
+          type: 'raster',
+          source: {
+            type: 'raster',
+            tiles: [
+              'https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=a41fb468ab8ad1c348ac85b9e99d4ffd',
+            ],
+          },
+        },
+        firstSymbolId
+      );
+    });
     // clean up on unmount
     return () => map.remove();
   }, [longitude, latitude]);
